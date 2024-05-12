00a0df950ac5128a2ffec53d7f6dbd24ec97071c
Devanshu046 <dev44908@gmail.com>
2024-05-12 13:12:00 +0530

    intial commit

--- routes.js
+++ routes.js
@@ -5,8 +5,11 @@ const app = express()
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+app.get('/profile', (req, res) => {
+  res.send('Hello user!')
+})
 app.get('/profile/:username', (req, res) => {
-  res.send(`Hello From ${req.profile.username}`)
+  res.send(`Hello From ${req.params.username}`)
 })
 
 app.listen(3000)
